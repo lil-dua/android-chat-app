2272b1162f1772d446438c2b83a6396c960e8053
hoang ryan <judaime36@gmail.com>
2022-09-19 15:50:41 +0700

    First pass

---
+++ app/src/main/java/tech/demoproject/android_chat_app/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package tech.demoproject.android_chat_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import tech.demoproject.android_chat_app.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
