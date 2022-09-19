7fa29a3ab875d71eb599f871b3dee2196001a62f
hoang ryan <judaime36@gmail.com>
2022-09-19 16:52:11 +0700

    Add SignInActivity.java and SignUpActivity.java

--- app/src/main/java/tech/demoproject/android_chat_app/activities/SignInActivity.java
+++ app/src/main/java/tech/demoproject/android_chat_app/activities/SignInActivity.java
@@ -2,15 +2,31 @@ package tech.demoproject.android_chat_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import tech.demoproject.android_chat_app.R;
+import tech.demoproject.android_chat_app.databinding.ActivitySignInBinding;
+
 
 public class SignInActivity extends AppCompatActivity {
+
+    // ActivitySignUpBinding class is automatically generated from our layout file: 'activity_sign_in'
+    private ActivitySignInBinding binding;  //View Binding : a feature that allows you to more easily write code that interface with views
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+
+    private void setListeners(){
+        // you can see: an instance of a binding class contains direct references to all view that have an ID in the corresponding layout
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
--- app/src/main/java/tech/demoproject/android_chat_app/activities/SignUpActivity.java
+++ app/src/main/java/tech/demoproject/android_chat_app/activities/SignUpActivity.java
@@ -2,15 +2,28 @@ package tech.demoproject.android_chat_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import tech.demoproject.android_chat_app.R;
+import tech.demoproject.android_chat_app.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    // ActivitySignUpBinding class is automatically generated from our layout file: 'activity_sign_up'
+    private ActivitySignUpBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        // you can see: an instance of a binding class contains direct references to all view that have an ID in the corresponding layout
+        binding.textSignIn.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignInActivity.class)));
     }
 }
