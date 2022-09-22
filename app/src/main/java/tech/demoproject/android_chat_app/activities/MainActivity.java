bfe71fc43b1403ac5f6a4e3e19547a3cd2cb1d3e
hoang ryan <judaime36@gmail.com>
2022-09-22 22:06:13 +0700

    Add func for SignUpActivity + store user info Firebase

--- app/src/main/java/tech/demoproject/android_chat_app/activities/MainActivity.java
+++ app/src/main/java/tech/demoproject/android_chat_app/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package tech.demoproject.android_chat_app;
+package tech.demoproject.android_chat_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import tech.demoproject.android_chat_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/tech/demoproject/android_chat_app/utilities/Constants.java
@@ -0,0 +1,15 @@
+package tech.demoproject.android_chat_app.utilities;
+
+/***
+ * Created by HoangRyan aka LilDua on 9/22/2022.
+ */
+public class Constants {
+    public static final String KEY_COLLECTION_USER = "user";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatAppPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignIn";
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+}
